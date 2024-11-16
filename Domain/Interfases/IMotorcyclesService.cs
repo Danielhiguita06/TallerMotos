6109b019fc036730a3a5f4f6b44ae681ee3eb9ae
Danielhiguita06 <danielstivenhiguitadavid@gmail.com>
2024-11-16 16:32:52 -0500

    IMotorcyclesService.cs Created. MotorcyclesService.cs Created.

---
+++ Domain/Interfases/IMotorcyclesService.cs
@@ -0,0 +1,17 @@
+﻿using TallerMotos.DAL.Entities;
+
+namespace TallerMotos.Domain.Interfaces
+{
+    public interface IMotorcyclesService
+    {
+        Task<IEnumerable<Motorcycles>> GetMotorciclesAsync();//Es una firma de un método
+
+        Task<Motorcycles> CreateMotorcyclesAsync(Motorcycles motorcycles);
+
+        Task<Motorcycles> GetMotorcyclesByPlate(String plate);
+
+        Task<Motorcycles> EditMotorcyclesAsync(String plate);
+
+        Task<Motorcycles> DeleteMotorcyclesAsync(String plate);
+    }
+}
